4fde0d54f28fef904fbdc0b56c53db05276108da
Mehmet Akbaba <mehmet.akbaba2011@gmail.com>
2022-08-06 01:28:45 +0300

    package using

--- lib/listview.dart
+++ lib/listview.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 class ListViewUsing extends StatelessWidget {
   ListViewUsing({Key? key}) : super(key: key);
@@ -14,21 +15,69 @@ class ListViewUsing extends StatelessWidget {
       appBar: AppBar(
         title: Text('Student List'),
       ),
-      body: ListView(
-        children: allStudent
-            .map(
-              (Student student) => ListTile(
-                title: Text(student.name),
-                subtitle: Text(student.surname),
-                leading: CircleAvatar(
-                  child: Text(
-                    student.id.toString(),
-                  ),
+      body: buildListViewSeperated(),
+    );
+  }
+
+  ListView buildListViewSeperated() {
+    return ListView.separated(
+      itemBuilder: (BuildContext context, int index) {
+        var students = allStudent[index];
+        return Card(
+          color:
+              index % 2 == 0 ? Colors.orange.shade100 : Colors.purple.shade100,
+          child: ListTile(
+            onTap: () {
+              if (index % 2 == 0) {
+                EasyLoading.instance.backgroundColor = Colors.red;
+              } else {
+                EasyLoading.instance.backgroundColor = Colors.yellow;
+              }
+              EasyLoading.showToast(
+                'Tıklandı...',
+                duration: Duration(
+                  seconds: 3,
+                ),
+                dismissOnTap: true,
+                toastPosition: EasyLoadingToastPosition.bottom,
+              );
+            },
+            title: Text(students.name),
+            subtitle: Text(students.surname),
+            leading: CircleAvatar(
+              child: Text(
+                students.id.toString(),
+              ),
+            ),
+          ),
+        );
+      },
+      itemCount: allStudent.length,
+      separatorBuilder: (context, index) {
+        var newIndex = index + 1;
+        if (newIndex % 4 == 0) {
+          return Divider(thickness: 2, color: Colors.teal);
+        }
+        return SizedBox();
+      },
+    );
+  }
+
+  ListView ClassicListView() {
+    return ListView(
+      children: allStudent
+          .map(
+            (Student student) => ListTile(
+              title: Text(student.name),
+              subtitle: Text(student.surname),
+              leading: CircleAvatar(
+                child: Text(
+                  student.id.toString(),
                 ),
               ),
-            )
-            .toList(),
-      ),
+            ),
+          )
+          .toList(),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:listtile/card_listtile.dart';
 import 'package:listtile/listview.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+  configLoading();
+}
+
+void configLoading() {
+  EasyLoading.instance
+    ..displayDuration = const Duration(milliseconds: 2000)
+    ..indicatorType = EasyLoadingIndicatorType.fadingCircle
+    ..loadingStyle = EasyLoadingStyle.custom
+    ..indicatorSize = 45.0
+    ..radius = 10.0
+    ..progressColor = Colors.yellow
+    ..backgroundColor = Colors.grey
+    ..indicatorColor = Colors.yellow
+    ..textColor = Colors.black
+    ..maskColor = Colors.blue.withOpacity(0.5)
+    ..userInteractions = true
+    ..dismissOnTap = true;
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -10,6 +30,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       home: ListViewUsing(),
+      builder: EasyLoading.init(),
     );
   }
 }
